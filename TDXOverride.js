1f174e27dc08028833c04e514c4f80a485a3a93f
kajih82 <62782146+kajih82@users.noreply.github.com>
2025-10-29 15:00:39 -0400

    Update TDXOverride.js

--- TDXOverride.js
+++ TDXOverride.js
@@ -1,1 +1,6 @@
 console.log('Hello World');
+// Get a reference to the button by its ID
+const myButton = document.getElementById('btnSubmit');
+
+// Hide the button
+myButton.style.display = 'none';
