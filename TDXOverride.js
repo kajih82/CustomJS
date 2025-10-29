0071cbc728d15ef7ddb6958a813e7375fad7812c
kajih82 <62782146+kajih82@users.noreply.github.com>
2025-10-29 14:51:43 -0400

    Update TDXOverride.js

--- TDXOverride.js
+++ TDXOverride.js
@@ -1,9 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Find the input element by ID
-    const btn = document.getElementById('btnSubmit');
-    
-    // Check if it exists and has the correct value
-    if (btn && btn.value === 'HideMe3000') {
-        btn.style.display = 'none'; // Hide the button
-    }
-});
+console.log('Hello World');
