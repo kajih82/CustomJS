c9bad4f7b65fd72716f91521859f822a5bab672a
kajih82 <62782146+kajih82@users.noreply.github.com>
2025-11-07 08:49:03 -0500

    Update TDXOverride.js

--- TDXOverride.js
+++ TDXOverride.js
@@ -1,13 +1,17 @@
 console.log('Start!');
 const style = document.createElement('style');
 style.innerHTML = `
+
+/* HIDE FORM BUTTONS IF NAMED HIDEME3000 */
 #btnSubmit[value="HideMe3000"] {
     display: none !important;
 }
 
+/* HIDE SIDEBAR FROM CATALOGUE AND SET MAIN CONTENT TO 100% */
 #servicesContent .row.gutter-top #divSidebar {
     display: none;
 }
+@media (min-width: 992px) { .col-md-8 { width: 100%; }
 
 `;
 document.head.appendChild(style);
